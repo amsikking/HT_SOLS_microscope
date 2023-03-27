e712c83623f915aceac2c569d084217d4532ddbc
Alfred Millett-Sikking <amsikking@gmail.com>
2023-03-27 16:26:58 -0700

    Update ht_sols_microscope_gui_objective_selector.py
    Swapped z drive from 'ZFM2030' to 'ZFM2020' to allow space for hardware autofocus. Re-calibrated positions for newly aligned BFP locations (no need to increase 'self._position_limit_um' with new setup). Tidied up code so it won't crash even if 'start_z_um' is not found in 'self.objective_BFP_um'.

--- ht_sols_microscope_gui_objective_selector.py
+++ ht_sols_microscope_gui_objective_selector.py
@@ -11,32 +11,30 @@ class ObjectiveSelector:
         self.z_drive_ch = 2
         self.z_controller = thorlabs_MCM3000.Controller(
             'COM12',
-            stages=(None, None, 'ZFM2030'),
-            reverse=(False, False, True),
+            stages=(None, None, 'ZFM2020'),
+            reverse=(False, False, False),
             verbose=False)
-        self.z_controller._position_limit_um = [None, None, 13100.0] # increase
         self.objectives = (
             'Nikon 40x0.95 air', 'Nikon 40x1.15 water', 'Nikon 40x1.30 oil')
         self.objective_BFP_um = ( # absolute position from alignment
-            -1039, -1175, -13062)
-        start_position = (len(self.objectives) + 1) # nothing selected for now
+            0, -137, -12023)
         start_z_um = self.z_controller.position_um[self.z_drive_ch]
-        if round(start_z_um) in self.objective_BFP_um: # check position
-            start_position = self.objective_BFP_um.index(round(start_z_um))
         frame = tk.LabelFrame(master, text='OBJECTIVE SELECTOR', bd=6)
         frame.grid(row=0, column=0, rowspan=1, padx=20, pady=20, sticky='n')
         self.objective = tki_cw.RadioButtons(
             frame,
             label='options',
             buttons=self.objectives,
-            default_position=start_position,
             function=self.function)
         quit_button = tk.Button(
             root, text="QUIT", command=self.quit, height=5, width=30)
         quit_button.grid(row=1, column=0, padx=20, pady=20, sticky='n')
         print('%s: -> done.'%self.name)
-        print('%s: current position   = %s'%(
-            self.name, self.objectives[start_position]))
+        if round(start_z_um) in self.objective_BFP_um: # check position
+            start_position = self.objective_BFP_um.index(round(start_z_um))
+            self.objective.tk_position.set(start_position)
+            print('%s: current position   = %s'%(
+                self.name, self.objectives[start_position]))
 
     def function(self, rb_pos):
         print('%s: moving to position = %s'%(
