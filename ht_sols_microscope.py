c2ebb7e9b589a395a4b3f192baba4e40fc1f542d
Alfred Millett-Sikking <amsikking@gmail.com>
2023-04-27 14:04:40 -0700

    Update ht_sols_microscope.py
    COM port updates + The default behavior for scipy.ndimage.zoom is to 'fill values beyond the edge' with 'cval' (=0 by default). So for certain combinations of camera chip cropping, scan range and voxel aspect ratio, an empty row (or column) of pixels would sometimes appear at the edge of the preview images. This was fine from a user perspective but was capable of upsetting the software autofocus routine (since it would confuse the 'sharp' edge from the zero pixel row as the max gradient of the fluorescent intensity!). The new argument mode='nearest' fills any empty pixels at the edges with the nearest pixel and should fix the problem.

--- ht_sols_microscope.py
+++ ht_sols_microscope.py
@@ -127,14 +127,14 @@ class Microscope:
     def _init_focus_piezo(self):
         if self.verbose: print("\n%s: opening focus piezo..."%self.name)
         self.focus_piezo = pi_E_709_1C1L.Controller(
-            which_port='COM3', z_min_um=0, z_max_um=800, verbose=False)
+            which_port='COM17', z_min_um=0, z_max_um=800, verbose=False)
         if self.verbose: print("\n%s: -> focus piezo open."%self.name)
         atexit.register(self.focus_piezo.close)
 
     def _init_XY_stage(self):
         if self.verbose: print("\n%s: opening XY stage..."%self.name)        
         self.XY_stage = pi_C_867_2U2.Controller(
-            which_port='COM6', verbose=False)
+            which_port='COM16', verbose=False)
         if self.verbose: print("\n%s: -> XY stage open."%self.name)
         atexit.register(self.XY_stage.close)
 
@@ -790,11 +790,14 @@ class DataPreview:
                 X_px_um = sample_px_um # width axis
                 Y_px_um = sample_px_um * np.cos(tilt) # prop. axis to scan axis
                 Z_px_um = sample_px_um * np.sin(tilt) # prop. axis to O1 axis
-                O1_img = zoom(O1_proj, (Y_px_um / X_px_um, 1))
-                scan_img = zoom(scan_proj, (Z_px_um / X_px_um, 1))
+                O1_img    = zoom(
+                    O1_proj, (Y_px_um / X_px_um, 1), mode='nearest')
+                scan_img  = zoom(
+                    scan_proj, (Z_px_um / X_px_um, 1), mode='nearest')
                 scan_scale = O1_img.shape[0] / width_proj.shape[0]
                 # = scan_step_size_um / X_px_um rounded to match O1_img.shape[0]
-                width_img = zoom(width_proj, (scan_scale, Z_px_um / X_px_um))
+                width_img = zoom(
+                    width_proj, (scan_scale, Z_px_um / X_px_um), mode='nearest')
                 # Make image with all projections and flip for traditional view:
                 y_px, x_px = O1_img.shape
                 line_min, line_max = O1_img.min(), O1_img.max()
