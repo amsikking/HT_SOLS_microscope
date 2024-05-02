14d4f64feb11d634b67be0710a48a00a26d2425d
Alfred Millett-Sikking <amsikking@gmail.com>
2024-05-02 14:42:02 -0700

    Update ht_sols_microscope_gui.py
    Minor update:
    - Removed unneeded args from 'apply_settings' since these are now set during the init of 'ht_sols_microscope.py'.
    - The attributes for 'focus_piezo' and 'XY_stage' can be taken from the scope object (no need to consult hardware attributes directly).

--- ht_sols_microscope_gui.py
+++ ht_sols_microscope_gui.py
@@ -69,17 +69,14 @@ class GuiMicroscope:
                 voxel_aspect_ratio   = self.voxel_aspect_ratio.value.get(),
                 scan_range_um        = self.scan_range_um.value.get(),
                 volumes_per_buffer   = self.volumes_per_buffer.value.get(),
-                autofocus_enabled    = False,
-                focus_piezo_z_um     = (0, 'relative'),
-                XY_stage_position_mm = (0, 0, 'relative'),
                 sample_ri            = self.sample_ri.value.get(),
                 ls_angular_dither_v  = self.ls_angular_dither.value.get(),
                 ).get_result() # finish
             # get XYZ direct from hardware and update gui to aviod motion:
             self.focus_piezo_z_um.update_and_validate(
-                int(round(self.scope.focus_piezo.z)))
+                int(round(self.scope.focus_piezo_z_um)))
             self._update_XY_stage_position(
-                [self.scope.XY_stage.x, self.scope.XY_stage.y])
+                self.scope.XY_stage_position_mm)
             # check microscope periodically:
             def _run_check_microscope():
                 self.scope.apply_settings().get_result() # update attributes
